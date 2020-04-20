0e1b32950e65f3cfd709dfb38e79df7d128d4e9d
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-04-20 14:17:44 +0200

    Added Italian translation (#3)
    * Added Italian translation

---
+++ lua/lang/italian/pharaoh.lua
@@ -0,0 +1,36 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[PHARAOH.name] = "Faraone"
+L["info_popup_" .. PHARAOH.name] = [[Sei il Faraone!
+Usa la tua Ankh a tuo vantaggio. Piazzala in una posizione strategica e assicurati che sia protetta!]]
+L["body_found_" .. PHARAOH.abbr] = "Era un Farone."
+L["search_role_" .. PHARAOH.abbr] = "Questa persona era un Faraone!"
+L["target_" .. PHARAOH.name] = "Faraone"
+L["ttt2_desc_" .. PHARAOH.name] = [[Il Faraone è un innocente che usa un oggetto speciale a suo vantaggio!]]
+
+L[GRAVEROBBER.name] = "Profanatore"
+L["info_popup_" .. GRAVEROBBER.name] = [[Sei il Profanatore!
+Cerca un'Ankh piazzata e prova ad usarla a tuo vantaggio!]]
+L["body_found_" .. GRAVEROBBER.abbr] = "Era un Profanatore."
+L["search_role_" .. GRAVEROBBER.abbr] = "Questa persona era un Profanatore!"
+L["target_" .. GRAVEROBBER.name] = "Profanatore"
+L["ttt2_desc_" .. GRAVEROBBER.name] = [[Il Profanatore è un giocatore che fa parte dei traditori che è l'avversario del Faraone.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_weapon_ankh_name"] = "Ankh"
+L["ttt2_weapon_ankh_desc"] = "Piazza quest'Ankh a terra."
+L["weapon_ttt_ankh_help"] = "Usa {primaryfire} per piazzare l'Ankh a terra."
+L["ankh_short_desc"] = "Un oggetto mistico a terra che permette ad alcuni player di respawnare."
+L["ankh_convert"] = "Tieni premuto [{usekey}] per convertire"
+L["ankh_unknown_terrorist"] = "Questa è un'Ankh di un traditore sconosciuto"
+L["ankh_no_pickup"] = "Non puoi raccogliere quest'Ankh"
+L["ankh_health_points"] = "HP: {health} / {maxhealth}"
+L["ankh_progress"] = "Progresso conversione: {progress}%"
+L["ankh_broken"] = "La tua Ankh è stata distrutta."
+L["ankh_broken_adv"] = "La tua Ankh è stata distrutta."
+L["ankh_no_traitor_alive"] = "L'Ankh non può essere posizionata se nessun traditore è vivo."
+L["ankh_too_steep"] = "Questa superficie è troppo ripida per piazzare un'Ankh."
+L["ankh_selected_graverobber"] = "Un traditore è stato convertito a profanatore. Ora è un tuo avversario!"
+L["ankh_popup_converted_title"] = "La tua Ankh è stata convertita!"
+L["ankh_popup_converted_text"] = "Il tuo avversario ha avuto abbastanza tempo per convertire la tua ankh ed usarlo per i suoi scopi. Tornaci ed assicurati che torni tua!"
